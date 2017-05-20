61029775512d3f3ed150a62ef0e2bc796a26803a
ilgarsh <ilgar.shahbazov@gmail.com>
2017-05-20 14:35:47 +0300

    join scss and js

---
+++ generate/src/scripts/modules/templatesEngine.js
@@ -0,0 +1,38 @@
+(function () {
+
+    var div = document.createElement('div');
+
+    function getTemplateRootNode(scriptId) {
+        var scriptTag = document.getElementById(scriptId);
+        div.innerHTML = scriptTag.innerHTML;
+        var result = div.children[0];
+        div.removeChild(result);
+        return result;
+    }
+
+    TODO_APP.modules.templatesEngine = {
+        todoItem: function (data) {
+            var root = getTemplateRootNode('todoItemTemplate');
+
+            var markReady = root.querySelector('.js-todo-item_mark-ready');
+            var removeAction = root.querySelector('.js-todo-item_remove-action');
+            var text = root.querySelector('.js-todo-item_text');
+
+            if (data.text) {
+                text.innerText = data.text;
+            }
+
+            if (data.isReady) {
+                markReady.checked = true;
+            }
+
+            return {
+                root: root,
+                text: text,
+                markReady: markReady,
+                removeAction: removeAction
+            };
+        }
+    }
+
+}());
---
+++ generate/src/scripts/utils/getTextNode.js
@@ -0,0 +1,23 @@
+(function () {
+    /**
+     * @param {HTMLElement} node
+     * @return {Node}
+     */
+    function getTextNode(node) {
+        var childs = node.childNodes;
+        var i = 0;
+        var l = childs.length;
+
+        for (; i !== l; i += 1) {
+            if (childs[i].nodeName === '#text') {
+                return childs[i];
+            }
+        }
+
+        var result = document.createTextNode('');
+        node.appendChild(result);
+        return result;
+    }
+
+    TODO_APP.utils.getTextNode = getTextNode;
+}());
